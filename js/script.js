93528fda4f190c8ce610099ecd8f0019af24db94
nbgka <nbogucka@neiu.edu>
2024-02-18 18:56:53 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let images = ["../media/images/candy.jpg","../media/images/cat.png","../media/images/ghost.jpg"];
+
+document.getElementById("randomNumber").addEventListener("click", displayImage);
+
+function displayImage(){
+    let image = document.getElementById('candy');
+    let random = Math.floor(Math.random() * images.length);
+    image.src = images[random];
+}
